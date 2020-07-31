191581cd14731716b0a6fd69fccb95fa49ff510c
Toni <toni@Tonis-MacBook-Pro.local>
2020-07-31 18:59:45 +0200

    up proj

--- App.js
+++ App.js
@@ -1,21 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container} >
+
+<WebView
+    originWhitelist={['*']}
+        source={{ uri: 'http://toni.asu-cs-gproj.tk' }}
+        style={{ marginTop: 40 }}
+      />
     </View>
+   
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#19b5fe',
   },
 });
