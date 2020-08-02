f61c26d45a83e1f075274508a5bd3563239c0779
Toni <toni@Tonis-MacBook-Pro.local>
2020-08-02 16:05:48 +0200

    add video conf feature in mobile

--- App.js
+++ App.js
@@ -1,17 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { WebView } from 'react-native-webview';
+import * as Permissions from 'expo-permissions';
+
 
 
 export default function App() {
+
+  useEffect(()=> {
+    checkAudiVideoPermission();
+  },[])
+  async function checkAudiVideoPermission() {
+    const { status, expires, permissions } = await Permissions.askAsync(
+      Permissions.CAMERA,
+      Permissions.AUDIO_RECORDING
+    );
+    if (status !== 'granted') {
+      alert('Hey! You have not enabled selected permissions');
+      
+    }
+  }
   return (
     <View style={styles.container} >
 
 <WebView
     originWhitelist={['*']}
-        source={{ uri: 'http://toni.asu-cs-gproj.tk' }}
+        source={{ uri: 'https://asu-cs-gproj.tk' }}
         style={{ marginTop: 40 }}
+        allowUniversalAccessFromFileURLs = {true}
+        allowFileAccess={true}
+
       />
     </View>
    
